61651292de72c13577b9fee461731217200e1348
Allan Gam <ssaweden@gmail.com>
2017-08-06 09:21:56 +0200

    Update ideAlarm.lua

--- scripts/ideAlarm.lua
+++ scripts/ideAlarm.lua
@@ -3,21 +3,7 @@ ideAlarm.lua
 
 Please read: https://github.com/allan-gam/ideAlarm/wiki
 
-Copyright (C) 2017  BakSeeDaa
-
-		This program is free software: you can redistribute it and/or modify
-		it under the terms of the GNU General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-		(at your option) any later version.
-
-		This program is distributed in the hope that it will be useful,
-		but WITHOUT ANY WARRANTY; without even the implied warranty of
-		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-		GNU General Public License for more details.
-
-		You should have received a copy of the GNU General Public License
-		along with this program.  If not, see <http://www.gnu.org/licenses
-
+Note that any changes to this file will be lost when upgrading.
 --]]
 
 package.path = globalvariables['script_path']..'modules/?.lua;'..package.path
